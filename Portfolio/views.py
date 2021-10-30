4e400c6835c5b465df5a7dad7d6225af12ff8e73
Zambianco <rodrigo.zambianco@yahoo.com.br>
2021-10-30 20:43:33 -0300

    Teste para gerar o Json

--- Portfolio/views.py
+++ Portfolio/views.py
@@ -1,7 +1,20 @@
 from django.shortcuts import render
 from .models import Bolo, Doce
+from django.http import JsonResponse
 
+def bolo_show(request):
+    data = list(Bolo.objects.values())  # wrap in list(), because QuerySet is not JSON serializable
+    return JsonResponse(data, safe=False)  # or JsonResponse({'data': data})
+
+
+
+'''
 def bolo_show(request):
     bolos = Bolo.objects.all()
     context = {'bolos': bolos}
     return render(request, 'bolos.html', context)
+
+def bolo_list(request):
+    data = list(Bolo.objects.values())  # wrap in list(), because QuerySet is not JSON serializable
+    return JsonResponse(data, safe=False)  # or JsonResponse({'data': data})
+'''
