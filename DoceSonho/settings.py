fe0fefa4225a0d9b734ba69c149ae77f09e88195
Zambianco <rodrigo.zambianco@yahoo.com.br>
2021-10-30 23:43:13 -0300

    configurada pasta para upload de imagens

--- DoceSonho/settings.py
+++ DoceSonho/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 ROOT_PATH = os.path.dirname(__file__)
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
-
+UPLOAD_PATH = os.path.join(BASE_DIR, 'frontend/static/upload')
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
--- Portfolio/models.py
+++ Portfolio/models.py
@@ -1,12 +1,12 @@
 from django.conf import settings
 from django.db import models
 
-
 class Bolo(models.Model):
     identificador = models.AutoField(primary_key=True)
     titulo = models.CharField(max_length=200)
     descricao = models.TextField()
     ingredientes = models.TextField()
+    foto = models.FileField(blank=True, upload_to=settings.UPLOAD_PATH)
 
     def publish(self):
         self.save()
@@ -20,6 +20,7 @@ class Doce(models.Model):
     titulo = models.CharField(max_length=200)
     descricao = models.TextField()
     ingredientes = models.TextField()
+    foto = models.FileField(blank=True, upload_to=settings.UPLOAD_PATH)
 
     def publish(self):
         self.save()
